f5a22b2a4f3d6f150b5b0abd54dd279b93899f98
deniz <deniz.temur57@gmail.com>
2024-11-13 18:23:47 +0300

    updated setup file

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="simplegrad",
+    version="0.0.1",
+    description="A simple autograd library",
+    author="Deniz",
     packages=find_packages(),
     package_data={
-        "simplegrad": ["*.so", "*.pyd"],
+        "py-simplegrad": ["*.so"],
     },
+    include_package_data=True,
 )
