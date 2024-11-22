8f58dbf1135e653aa829b0de5ea71c04013ab5f1
deniz <deniz.temur57@gmail.com>
2024-11-22 15:54:42 +0300

    masked bindings for quick iterations

--- src-simplegrad/simplegrad.cpp
+++ src-simplegrad/simplegrad.cpp
@@ -1,29 +1,31 @@
 #include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
 
-#include "include/space.h"
+#include "include/node.h"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(simplegrad, m) {
-    m.doc() = "Auto gradient module";  // Module docstring
-    py::class_<Space>(m, "Space")
+    m.doc() = "Automatic differentation module written in C++";  // Module docstring
+    py::class_<Node>(m, "Node")
         .def(py::init<float>())
         .def(py::self + py::self)
         .def(py::self + float())
+        /*
         .def(py::self * py::self)
-        .def("__pow__", &Space::pow)
-        .def("__neg__", &Space::neg)
-        .def("__sub__", &Space::sub)
-        .def("__truediv__", &Space::truediv)
-        .def("__radd__", &Space::radd)
-        .def("__rsub__", &Space::rsub)
-        .def("__rmul__", &Space::rmul)
-        .def("__rtruediv__", &Space::rtruediv)
-        .def("__repr__", &Space::print)
-        .def("relu", &Space::relu)
-        .def("backward", &Space::backward)
-        .def("data", (float(Space::*)()) & Space::data)
-        .def("grad", (float(Space::*)()) & Space::grad)
-        .def("op", (const std::string& (Space::*)()) & Space::op);
+        .def("__pow__", &Node::pow)
+        .def("__neg__", &Node::neg)
+        .def("__sub__", &Node::sub)
+        .def("__truediv__", &Node::rdiv)
+        .def("__radd__", &Node::radd)
+        .def("__rsub__", &Node::rsub)
+        .def("__rmul__", &Node::rmul)
+        .def("__rtruediv__", &Node::rtruediv)
+        .def("relu", &Node::relu)
+        */
+        .def("__repr__", &Node::print)
+        .def("backward", &Node::backward)
+        .def("data", &Node::get_data)
+        .def("grad", &Node::get_grad)
+        .def("op", &Node::get_op);
 }
