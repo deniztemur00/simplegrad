d60fc5c2a428f3d4713eba172daaf94f7936c9ba
deniz <deniz.temur57@gmail.com>
2024-11-26 15:58:43 +0300

    feat: Introduced shared_ptr to base binding class.

--- src-simplegrad/simplegrad.cpp
+++ src-simplegrad/simplegrad.cpp
@@ -1,5 +1,6 @@
 #include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include "include/node.h"
 
@@ -7,25 +8,27 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(simplegrad, m) {
     m.doc() = "Automatic differentation module written in C++";  // Module docstring
-    py::class_<Node>(m, "Node")
+    py::class_<Node, std::shared_ptr<Node>>(m, "Node")
         .def(py::init<float>())
-        .def(py::self + py::self)
-        .def(py::self + float())
-        /*
-        .def(py::self * py::self)
-        .def("__pow__", &Node::pow)
-        .def("__neg__", &Node::neg)
-        .def("__sub__", &Node::sub)
-        .def("__truediv__", &Node::rdiv)
-        .def("__radd__", &Node::radd)
-        .def("__rsub__", &Node::rsub)
-        .def("__rmul__", &Node::rmul)
-        .def("__rtruediv__", &Node::rtruediv)
-        .def("relu", &Node::relu)
-        */
         .def("__repr__", &Node::print)
         .def("backward", &Node::backward)
         .def("data", &Node::get_data)
         .def("grad", &Node::get_grad)
-        .def("op", &Node::get_op);
+        .def("prev", &Node::get_prev)
+        .def("op", &Node::get_op)
+        .def(py::self + py::self)
+        .def(py::self + float())
+        .def(float() + py::self);
+    /*
+    .def(py::self * py::self)
+    .def("__pow__", &Node::pow)
+    .def("__neg__", &Node::neg)
+    .def("__sub__", &Node::sub)
+    .def("__truediv__", &Node::rdiv)
+    .def("__radd__", &Node::radd)
+    .def("__rsub__", &Node::rsub)
+    .def("__rmul__", &Node::rmul)
+    .def("__rtruediv__", &Node::rtruediv)
+    .def("relu", &Node::relu)
+    */
 }
