ed971992145c7387b02398c84d5baef58f8bc2cc
deniz <deniz.temur57@gmail.com>
2024-11-27 21:04:02 +0300

    feat: Bind Module, Neuron, Layer, and MLP classes to Python using pybind11

--- src-simplegrad/simplegrad.cpp
+++ src-simplegrad/simplegrad.cpp
@@ -2,6 +2,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
+#include "include/net.h"
 #include "include/node.h"
 
 namespace py = pybind11;
@@ -32,4 +33,32 @@ PYBIND11_MODULE(simplegrad, m) {
         .def("__pow__", static_cast<NodePtr (Node::*)(const Node&) const>(&Node::pow))
         .def("__truediv__", &Node::rdiv)
         .def("__rtruediv__", &Node::rtruediv);
+
+    // Bind Module base class
+    py::class_<Module, std::shared_ptr<Module>>(m, "Module")
+        .def("zero_grad", &Module::zero_grad)
+        .def("parameters", &Module::parameters);
+
+    // Bind Neuron class
+    py::class_<Neuron, Module, std::shared_ptr<Neuron>>(m, "Neuron")
+        .def(py::init<int, bool>(),
+             py::arg("nin"),
+             py::arg("nonlin") = true)
+        .def("__call__", &Neuron::operator())
+        .def("parameters", &Neuron::parameters)
+        .def("__repr__", &Neuron::display_params);
+
+    // Bind Layer class
+    py::class_<Layer, Module, std::shared_ptr<Layer>>(m, "Layer")
+        .def(py::init<int, int>())
+        .def("__call__", &Layer::operator())
+        .def("parameters", &Layer::parameters)
+        .def("__repr__", &Layer::display_params);
+
+    // Bind MLP class
+    py::class_<MLP, Module, std::shared_ptr<MLP>>(m, "MLP")
+        .def(py::init<int, std::vector<int>>())
+        .def("__call__", &MLP::operator())
+        .def("parameters", &MLP::parameters)
+        .def("__repr__", &MLP::display_params);
 }
