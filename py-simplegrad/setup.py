7f2f07fc566350bf66443a7e132bddfda755b1d0
deniz <deniz.temur57@gmail.com>
2024-11-29 15:48:45 +0300

    feat: Bump version to 0.1.2 and update description; include .pyi files in package data

--- py-simplegrad/setup.py
+++ py-simplegrad/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="simplegrad",
-    version="0.1.1",
-    description="A simple autograd library",
+    version="0.1.2",
+    description="Automatic differentiation library for basic arithmetic operations",
     author="Deniz",
     packages=find_packages(),
     package_data={
-        "simplegrad": ["*.so"],
+        "simplegrad": ["*.so", "*.pyi"],
     },
     include_package_data=True,
 )
