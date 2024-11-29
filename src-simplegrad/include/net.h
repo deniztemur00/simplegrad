2591bcb268b92e6c7177654b1b4dd95544339162
deniz <deniz.temur57@gmail.com>
2024-11-29 15:54:27 +0300

    refactor: Remove MLP class implementation from header and source files

--- src-simplegrad/include/net.h
+++ src-simplegrad/include/net.h
@@ -40,15 +40,3 @@ class Layer : public Module {
     std::string display_params();
 };
 
-class MLP : public Module {
-   private:
-    std::vector<Layer> layers;
-    int n_params;
-
-   public:
-    MLP(int nin, std::vector<int> nouts);
-    NodePtrVec operator()(NodePtrVec& x);
-    NodePtrVec operator()(const pybind11::array_t<float>& x);  // numpy support
-    NodePtrVec parameters() override;
-    std::string display_params();
-};
