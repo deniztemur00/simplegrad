afec89f325c9426916627435bdafc00e8227d653
deniz <deniz.temur57@gmail.com>
2024-12-02 17:55:08 +0300

    feat: Add begin() and end() methods for Neuron and Layer classes

--- src-simplegrad/include/net.h
+++ src-simplegrad/include/net.h
@@ -22,6 +22,10 @@ class Neuron : public Module {
     bool nonlin;
 
    public:
+    auto begin() { return weights.begin(); }
+    auto end() { return weights.end(); }
+    auto begin() const { return weights.begin(); }
+    auto end() const { return weights.end(); }
     Neuron(int nin, bool nonlin = true);
     NodePtr operator()(NodePtrVec& x);
     NodePtrVec parameters() override;
@@ -34,9 +38,12 @@ class Layer : public Module {
     int n_params;
 
    public:
+    auto begin() { return neurons.begin(); }
+    auto end() { return neurons.end(); }
+    auto begin() const { return neurons.begin(); }
+    auto end() const { return neurons.end(); }
     Layer(int nin, int nouts);
     NodePtrVec operator()(NodePtrVec& x);
     NodePtrVec parameters() override;
     std::string display_params();
 };
-
