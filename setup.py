b246f8edee671575529cff988010607680a4ec16
deniz <deniz.temur57@gmail.com>
2024-12-10 17:27:37 +0300

    chore: Update setup.py for version bump and package structure changes

--- setup.py
+++ setup.py
@@ -1,45 +1,19 @@
 from setuptools import setup, find_packages, Extension
-from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-ext_modules = [
-    Pybind11Extension(
-        "simplegrad",
-        sources=[
-            "src-simplegrad/simplegrad.cpp",
-            "src-simplegrad/src/mlp.cpp",
-            "src-simplegrad/src/net.cpp",
-            "src-simplegrad/src/node.cpp",
-        ],
-        include_dirs=[
-            "src-simplegrad/include",
-        ],
-    ),
-]
 
 setup(
     name="simplegrad",
-    version="0.0.42",
+    version="0.0.43",
     description="Automatic differentiation library for basic arithmetic operations",
     author="Deniz",
     url="https://github.com/deniztemur00/simplegrad.git",
     long_description=open("MANIFEST.md").read(),
     long_description_content_type="text/markdown",
-    packages=find_packages(),
+    package_dir={"simplegrad": "py-simplegrad/simplegrad-dev"},
+    packages=["simplegrad"],
     package_data={
-        "simplegrad": ["*.pyi", "py.typed"],
+        "simplegrad": ["*.pyi", "py.typed", "*.so", "__init__.py"],
     },
-    data_files=[
-        (
-            "simplegrad",
-            [
-                "py-simplegrad/simplegrad/simplegrad.pyi",
-                "py-simplegrad/simplegrad/py.typed",
-            ],
-        )
-    ],
-    ext_modules=ext_modules,
-    cmdclass={"build_ext": build_ext},
-    zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
